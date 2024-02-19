4af86cc8f4e90acac0e762152b59477031bbb422
Matviienko Ruslana <brauniesrus@gmail.com>
2024-02-19 21:58:42 +0200

    card

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -22,8 +22,49 @@ const Modal = ({ onClose, car }) => {
 
   return (
     <div className="overlay" onClick={onClose}>
-      <div className="moda">
-        <img className="image" src={car.img} alt={car.make} />
+      <div className="modal">
+        <img
+          className="modal-image-car"
+          src={car.img ? car.img : car.photoLink}
+          alt={car.make}
+        />
+        <div className="modal-first-box-aboutCar">
+          <div className="modal-item-box1">
+            <p className="modal-text-Make">{car.make}</p>
+            <p className="modal-text-model">{car.model}</p>
+            <p className="modal-text-year">{car.year}</p>
+          </div>
+        </div>
+        <div className="modal-second-box-aboutCar">
+          <div className="modal-item-box2">
+            <p>{car.address.split(",")[1]} |</p>
+            <p>{car.address.split(",")[2]} |</p>
+            <p>{car.rentalCompany} |</p>
+
+            {car.accessories.some(
+              (str) => str.toLocaleLowerCase().indexOf("premium") >= 0
+            ) && <p>Premium</p>}
+          </div>
+          <div className="modal-item-box2">
+            <p>{car.type}</p>
+            <p>{car.model}</p>
+            <p>{car.year}</p>
+            <p>{car.type}</p>
+          </div>
+        </div>
+        <div className="modal-third-box-aboutCar">
+          <p className="modal-text-description">{car.description}</p>
+        </div>
+        <div className="modal-fourth-box-aboutCar">
+          <p className="modal-text-accesories">
+            Accesories and functionalities:
+          </p>
+        </div>
+        <div className="modal-sixth-box-aboutCar">
+          <p className="modal-list-accesories">
+            Accesories and functionalities:
+          </p>
+        </div>
       </div>
     </div>
   );
