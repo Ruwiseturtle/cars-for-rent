4b95043721b4ef549a8d8ebab528d47f106b267f
Matviienko Ruslana <brauniesrus@gmail.com>
2024-02-23 21:15:07 +0200

    background

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./HomePage.css";
 
 const HomePage = () => {
- 
-
   return (
     <div className="box-text">
-      <h1 className="title-text">Fast & Easy way to rent a car</h1>
-      <h2 className="description-text">
+      <p className="title-text">Fast & Easy way to rent a car</p>
+      <p className="description-text">
         A fleet of more than 200 car classes and for every pocket allows us to
         provide website clients with the best and most profitable offers for car
         booking and rental.
-      </h2>
+      </p>
     </div>
   );
 };
