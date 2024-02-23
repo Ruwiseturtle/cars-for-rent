f52961c32bd6cd4205cc369f8ed66c71854d994e
Matviienko Ruslana <brauniesrus@gmail.com>
2024-02-23 20:50:37 +0200

    modal finished

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -26,18 +26,65 @@ const Modal = ({ onClose, car }) => {
           alt={car.make}
         />
         <ul className="info-car-box">
-          <li className="style-text1">sss</li>
-          <li className="style-text2">sss</li>
-          <li className="style-text2">sss</li>
-          <li className="style-text3 ">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
-          <li className="">sss</li>
+          <li className="list-item info1">
+            <p className="style-text1">{car.make} &nbsp;</p>
+            <p className="style-text1 blue">{car.model} &nbsp;</p>
+            <p className="style-text1">{car.year}</p>
+          </li>
+          <li className="list-item info2">
+            <p className="style-text2">{car.address.split(",")[1]} |</p>
+            <p className="style-text2">{car.address.split(",")[2]} |</p>
+            <p className="style-text2">id: {car.id} |</p>
+            <p className="style-text2">year: {car.year} |</p>
+            <p className="style-text2">type: {car.type} </p>
+          </li>
+          <li className="list-item info3">
+            <p className="style-text2">
+              Fuel Consumption: {car.fuelConsumption} |
+            </p>
+            <p className="style-text2">Engine Size: {car.engineSize}</p>
+          </li>
+          <li className="list-item info4">
+            <p className="style-text3">{car.description}</p>
+          </li>
+          <li className="list-item info5">
+            <p className="style-text5">Accesories and functionalities:</p>
+          </li>
+          <li className="list-item info6">
+            {car.accessories.map((accessorie) => (
+              <p className="style-text2">{accessorie}&nbsp; |&nbsp;</p>
+            ))}
+          </li>
+          <li className="list-item info7">
+            {car.functionalities.map((functionalitie) => (
+              <p className="style-text2">{functionalitie} &nbsp;|&nbsp;</p>
+            ))}
+          </li>
+          <li className="list-item info8">
+            <p className="style-text5">Rental Conditions:</p>
+          </li>
+          <li className="list-item ">
+            <p className="style-text4 Minimum-age">Minimum age: </p>
+            <p className="style-text4 blue num-minimum-age">
+              {car.rentalConditions.slice(12, 15)}
+            </p>
+            <p className="style-text4 rental-condition">
+              {car.rentalConditions.slice(16, 38)}
+            </p>
+          </li>
+          <li className="list-item info9">
+            <p className="style-text4 rental-condition">
+              {car.rentalConditions.slice(39)}
+            </p>
+            <p className="style-text4 mileage">Mileage: </p>
+            <p className="style-text4 blue num-mileage">{car.mileage}</p>
+            <p className="style-text4 price">Price: </p>
+            <p className="style-text4 blue rentalPrice">
+              {car.rentalPrice.slice(1)}$
+            </p>
+          </li>
         </ul>
+        <button className="btn-rental-car">Rental car</button>
       </div>
     </div>
   );
