08d0e691e3150a18947ce595116fffad522bea95
Matviienko Ruslana <brauniesrus@gmail.com>
2024-03-14 19:42:45 +0200

    loader

--- src/API/cars/gerCars.jsx
+++ src/API/cars/gerCars.jsx
@@ -5,7 +5,7 @@ axios.defaults.baseURL = "https://6538cb3ca543859d1bb1ed9f.mockapi.io/";
 
 export const requestGetAllCars = async (page) => {
   try {
-    const { data } = await axios.get(`carsaaa?page=${page}&limit=${PER_PAGE}`);
+    const { data } = await axios.get(`cars?page=${page}&limit=${PER_PAGE}`);
     return data;
   } catch (e) {
     throw new Error(e);
